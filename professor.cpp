45c33fcff4ed968ef3cfa55e11785ccef7130abe
Habiba-Mohsen <101303283+Habiba-Mohsen@users.noreply.github.com>
2022-12-14 01:31:33 +0200

    edits by habiba

--- professor.cpp
+++ professor.cpp
@@ -3,3 +3,41 @@
 //
 
 #include "professor.h"
+#include <iostream>
+using namespace std;
+void professor::setName(const string &name) {
+        Name = name;
+    }
+
+void professor::setCode(const string &code) {
+    Code=code;
+
+}
+
+void professor::setLectureHall(const string &lectureHall) {
+        LectureHall = lectureHall;
+
+}
+
+void professor::setTime(const string &time) {
+        Time = time;
+
+}
+
+const string &professor::getName() {
+
+        return Name;
+
+}
+
+const string &professor::getCode() {
+    return Code;
+}
+
+const string &professor::getLectureHall() {
+    return LectureHall;
+}
+
+const string &professor::getTime() {
+    return Time;
+}
--- professor.h
+++ professor.h
@@ -4,10 +4,32 @@
 
 #ifndef LEARNING_MANAGEMENT_SYSTEM_PROFESSOR_H
 #define LEARNING_MANAGEMENT_SYSTEM_PROFESSOR_H
+#include <iostream>
+using namespace std;
+
+class professor { private:
+    string Name;
+    string Code;
+    string LectureHall;
+    string Time;
+public:
+    // Setter
+    void setName(const string &name);
 
 
-class professor {
+    void setCode(const string &code);
 
+    void setLectureHall(const string &lectureHall) ;
+
+    void setTime(const string &time) ;
+
+    // Getter
+    const string &getName() ;
+
+    const string &getCode() ;
+    const string &getLectureHall();
+
+    const string &getTime() ;
 };
 
 
